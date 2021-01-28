3ab3c9a08735557d86581fa6d8816b3c6e13505b
安栋 <ad990606@163.com>
2021-01-28 15:13:30 +0800

    完成

--- src/api/user.js
+++ src/api/user.js
@@ -2,7 +2,7 @@
  * 用户相关请求模块
  */
 import request from '@/utils/request'
-import store from '@/store'
+// import store from '@/store'
 
 export const login = data => {
   return request({
@@ -29,13 +29,13 @@ export const sendSms = mobile => {
 export const getUserInfo = () => {
   return request({
     method: 'GET',
-    url: '/app/v1_0/user',
+    url: '/app/v1_0/user'
     // 发送请求头数据
-    headers: {
-      // 注意：该接口需要授权才能访问
-      //       token的数据格式：Bearer token数据，注意 Bearer 后面有个空格
-      Authorization: `Bearer ${store.state.user.token}`
-    }
+    // headers: {
+    //   // 注意：该接口需要授权才能访问
+    //   //       token的数据格式：Bearer token数据，注意 Bearer 后面有个空格
+    //   Authorization: `Bearer ${store.state.user.token}`
+    // }
   })
 }
 
@@ -46,5 +46,60 @@ export const getUserChannels = () => {
   return request({
     method: 'GET',
     url: '/app/v1_0/user/channels'
+  })
+}
+
+/**
+ * 关注用户
+ */
+export const addFollow = target => {
+  return request({
+    method: 'POST',
+    url: '/app/v1_0/user/followings',
+    data: {
+      target
+    }
+  })
+}
+
+/**
+ * 取消关注用户
+ */
+export const deleteFollow = target => {
+  return request({
+    method: 'DELETE',
+    url: `/app/v1_0/user/followings/${target}`
+  })
+}
+
+/**
+ * 获取当前登录用户的个人资料
+ */
+export const getUserProfile = target => {
+  return request({
+    method: 'GET',
+    url: '/app/v1_0/user/profile'
+  })
+}
+
+/**
+ * 更新用户资料
+ */
+export const updateUserProfile = data => {
+  return request({
+    method: 'PATCH',
+    url: '/app/v1_0/user/profile',
+    data
+  })
+}
+
+/**
+ * 更新用户照片资料
+ */
+export const updateUserPhoto = data => {
+  return request({
+    method: 'PATCH',
+    url: '/app/v1_0/user/photo',
+    data
   })
 }
